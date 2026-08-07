d970a55a5b4f39c797bc36526f1de7eb011e01f6
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-08-07 20:06:18 -0300

    Test automatic Caracol catalog discovery

--- local_offers/tests/test_sources.py
+++ local_offers/tests/test_sources.py
@@ -1,4 +1,4 @@
-from app.sources import extract_heyzine_info, sha256_bytes
+from app.sources import discover_caracol_catalog_url, extract_heyzine_info, sha256_bytes
 
 
 def test_extract_heyzine_info():
@@ -10,6 +10,20 @@ def test_extract_heyzine_info():
     assert info["pdf_filename"] == "v3/demo.pdf"
     assert info["title"] == "04.08 al 17.08"
     assert info["pdf_urls"][1].endswith("/files/uploaded/v3/demo.pdf")
+
+
+def test_discover_caracol_catalog_strips_tracking():
+    html = '''
+    <a class="catalogo" href="https://heyzine.com/flip-book/fafe2791cf.html?fbclid=tracking">Catálogo</a>
+    '''
+    assert discover_caracol_catalog_url(html) == "https://heyzine.com/flip-book/fafe2791cf.html"
+
+
+def test_discover_caracol_catalog_handles_html_entities():
+    html = '''
+    <a href="https://heyzine.com/flip-book/abc123.html?x=1&amp;y=2">Ver ofertas</a>
+    '''
+    assert discover_caracol_catalog_url(html) == "https://heyzine.com/flip-book/abc123.html"
 
 
 def test_sha256():
